4413160dffb4c214130c04de53a84c27c34b2168
fabianosantos <fabianosantos.contato@gmail.com>
2015-12-04 01:30:07 -0200

    Commit inicial do projeto com SpringBoot

---
+++ src/main/java/com/wyt/WashYourTravelApplication.java
@@ -0,0 +1,12 @@
+package com.wyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashYourTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashYourTravelApplication.class, args);
+    }
+}
